7fa52ff3249998d3b030ea0a6a8efca86193021f
nayaresoledad <olealpaca@gmail.com>
2022-10-21 11:05:02 +0200

    Subiendo script de acceso a database

---
+++ config/db.py
@@ -0,0 +1,17 @@
+from sqlalchemy import create_engine, MetaData
+import sqlalchemy
+
+from config.settings import Settings
+
+settings = Settings()
+
+DB_NAME = settings.db_name
+DB_USER = settings.db_user
+DB_PASS = settings.db_pass
+DB_HOST = settings.db_host
+DB_PORT = settings.db_port
+
+engine = sqlalchemy.create_engine(f"postgresql+psycopg2://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}")
+
+meta_data = MetaData()
+
