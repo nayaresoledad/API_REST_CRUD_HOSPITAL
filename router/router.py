9b81a4d0f02c417b0e42afe83cc9b940f1e5a874
= <=>
2022-11-10 15:01:46 +0100

    Se añaden mas loggs

--- router/router.py
+++ router/router.py
@@ -1,4 +1,5 @@
 from datetime import date
+import os
 from urllib import response
 from fastapi import APIRouter, Response, HTTPException
 from schema.paciente_schema import PacienteSchema
@@ -14,17 +15,18 @@ import logging
 
 #Configuramos un logger para trackear las acciones en el CRUD
 logging.basicConfig(
-    level=logging.DEBUG, #if os.environ.get("DEBUG_MODE") == "1" else logging.INFO, 
+    level=logging.DEBUG, #if os.environ.get("DEBUG_MODE") == "1" else logging.INFO,
     filename="logging_record.log", 
-    filemode="a",
+    filemode="w", #Cmabiar a "a" cuando no se quiera sobreescribir los logs
     format="%(asctime)s - %(levelname)s - %(message)s")
 
 #Guardamos la función router en una variable para usarla después.
 user = APIRouter()
 
 #Página principal, la bienvenida a nuestro hospital
-@user.get("/", tags=["pagina principal"])  #Indicamos aqui la ruta en la que se hará la consulta (get), y el tag apropiado para esa ruta
+@user.get("/", tags=["pagina principal"],status_code=200)  #Indicamos aqui la ruta en la que se hará la consulta (get), y el tag apropiado para esa ruta
 async def root():
+    logging.info("Se ha entrado en la raiz")
     return "Esto es Hospital F5"
 
 
@@ -36,6 +38,7 @@ async def get_pacientes():
  with engine.connect() as conn:                           #Usamos el entorno with para abrir la conexión con la DB sólo cuando es necesaria (buenas prácticas). Tras usarse, la conexión se cierra.
     result = conn.execute(paciente.select()).fetchall()
     logging.info("Se ha consultado los pacientes que hay")
+    logging.debug(f"Estos son los pacientes que se han consultado: {result}")
     return result
 
 #Buscar paciente por id
@@ -43,18 +46,19 @@ async def get_pacientes():
 async def get_paciente(paciente_id: int):
     with engine.connect() as conn:
         result = conn.execute(paciente.select().where(paciente.c.id_paciente == paciente_id)).first() #La función first hace que nos devuelva el primer elemento de la lista, ya que solo buscamos un paciente
-        logging.debug(result)
+        logging.debug(f"Paciente que se intenta buscar:{result}")
         if result == None:
+            logging.error(f"Se ha buscado el id de paciente {paciente_id} pero no existe")
             raise HTTPException(status_code=404, detail="Paciente no encontrado")
-        
-        logging.info(f"Se ha consultado el pacientes con el id {paciente_id}")
-        return result    #Devuelve la información del paciente buscado en forma de diccionario 
+        logging.info(f"Se ha buscado al paciente con el id {paciente_id}")
+        return result    #Devuelve la información del paciente buscado en forma de diccionario
 
 #Añadir nuevos pacientes a la base de datos
 @user.post("/pacientes", status_code=HTTP_201_CREATED, tags=["Pacientes"])
 async def crear_paciente(data_paciente: PacienteSchema):                      #Basamos los nuevos datos en el esquema de Paciente, creado en la carpeta schema
     with engine.connect() as conn:
-        nuevo_paciente = data_paciente.dict()                           #Creamos un diccionario con los datos del nuevo paciente
+        nuevo_paciente = data_paciente.dict()   #Creamos un diccionario con los datos del nuevo paciente
+        logging.debug(f"Se intenta crear un paciente con {nuevo_paciente}")
         conn.execute(paciente.insert().values(nuevo_paciente))          #Conectamos a la DB e insertamos el nuevo paciente
         logging.info(f"Se ha creado un paciente con {nuevo_paciente}")
         return Response(status_code=HTTP_201_CREATED)                   #Hacemos que cuando sea exitoso devuelva un código 200 CREADO
@@ -64,7 +68,10 @@ async def crear_paciente(data_paciente: PacienteSchema):                      #B
 async def actualizar_paciente(data_update: PacienteSchema, paciente_id: int):
     with engine.connect() as conn:
         result = conn.execute(paciente.update().where(paciente.c.id_paciente == paciente_id).values(id_paciente=data_update.id_paciente, nombre_paciente=data_update.nombre_paciente, apellidos_paciente=data_update.apellidos_paciente, numero_historial_clinico=data_update.numero_historial_clinico, domicilio=data_update.domicilio, poblacion_provincia=data_update.poblacion_provincia, telefono=data_update.telefono))
+        logging.debug(f"Se intentan modificar los datos del paciente {paciente_id}")
         result = conn.execute(paciente.select().where(paciente.c.id_paciente == paciente_id)).first()
+        logging.debug(f"Estos son los datos que se introducen {result}")
+        logging.info(f"Se han modificado los datos del paciente con id {paciente_id}")
         return result
 
 #Borrar un paciente
@@ -72,6 +79,7 @@ async def actualizar_paciente(data_update: PacienteSchema, paciente_id: int):
 async def borrar_paciente(paciente_id: int):
     with engine.connect() as conn:
         conn.execute(paciente.delete().where(paciente.c.id_paciente == paciente_id))           #Borra el paciente en el que la columna (c) id paciente es la introducida
+        logging.info(f"Se han borrado los datos del paciente con id {paciente_id}")
         return Response(status_code=HTTP_204_NO_CONTENT)
 
 
