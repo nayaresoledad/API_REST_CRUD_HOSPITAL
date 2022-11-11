8984fe83de0e83735d5da426c90e103402de9493
nayaresoledad <olealpaca@gmail.com>
2022-11-11 09:54:47 +0100

    Añadiendo requerimientos y summary de los endpoints en router

--- router/router.py
+++ router/router.py
@@ -23,7 +23,7 @@ logging.basicConfig(
 user = APIRouter()
 
 #Página principal, la bienvenida a nuestro hospital
-@user.get("/", tags=["pagina principal"],status_code=200)  #Indicamos aqui la ruta en la que se hará la consulta (get), y el tag apropiado para esa ruta
+@user.get("/", tags=["pagina principal"],status_code=200, summary="Página principal")  #Indicamos aqui la ruta en la que se hará la consulta (get), y el tag apropiado para esa ruta
 async def root():
     logging.info("Se ha entrado en la raiz")
     return "Esto es Hospital F5"
@@ -32,7 +32,7 @@ async def root():
 ###Tabla Pacientes
 
 #Buscar todos los pacientes
-@user.get("/pacientes", response_model=List[PacienteSchema], tags=["Pacientes"])
+@user.get("/pacientes", response_model=List[PacienteSchema],summary="Consulta todos los pacientes", tags=["Pacientes"])
 async def get_pacientes():
  with engine.connect() as conn:                           #Usamos el entorno with para abrir la conexión con la DB sólo cuando es necesaria (buenas prácticas). Tras usarse, la conexión se cierra.
     result = conn.execute(paciente.select()).fetchall()
@@ -41,7 +41,7 @@ async def get_pacientes():
     return result
 
 #Buscar paciente por id
-@user.get("/pacientes/{paciente_id}", response_model=PacienteSchema, tags=["Pacientes"]) #En este caso a la direccion paciente le añadiremos un id
+@user.get("/pacientes/{paciente_id}", response_model=PacienteSchema, summary="Consulta el paciente con el id introducido", tags=["Pacientes"]) #En este caso a la direccion paciente le añadiremos un id
 async def get_paciente(paciente_id: int):
     with engine.connect() as conn:
         result = conn.execute(paciente.select().where(paciente.c.id_paciente == paciente_id)).first() #La función first hace que nos devuelva el primer elemento de la lista, ya que solo buscamos un paciente
@@ -53,7 +53,7 @@ async def get_paciente(paciente_id: int):
         return result    #Devuelve la información del paciente buscado en forma de diccionario
 
 #Añadir nuevos pacientes a la base de datos
-@user.post("/pacientes", status_code=HTTP_201_CREATED, tags=["Pacientes"])
+@user.post("/pacientes", status_code=HTTP_201_CREATED, tags=["Pacientes"], summary="Añade un nuevo paciente")
 async def crear_paciente(data_paciente: PacienteSchema):                      #Basamos los nuevos datos en el esquema de Paciente, creado en la carpeta schema
     with engine.connect() as conn:
         nuevo_paciente = data_paciente.dict()   #Creamos un diccionario con los datos del nuevo paciente
@@ -63,7 +63,7 @@ async def crear_paciente(data_paciente: PacienteSchema):                      #B
         return Response(status_code=HTTP_201_CREATED)                   #Hacemos que cuando sea exitoso devuelva un código 200 CREADO
 
 #Actualizar información de un paciente ya existente
-@user.put("/pacientes/{paciente_id}", status_code=HTTP_202_ACCEPTED, tags=["Pacientes"])
+@user.put("/pacientes/{paciente_id}", status_code=HTTP_202_ACCEPTED, tags=["Pacientes"], summary="Actualiza la información de un paciente ya existente")
 async def actualizar_paciente(data_update: PacienteSchema, paciente_id: int):
     with engine.connect() as conn:
         result = conn.execute(paciente.update().where(paciente.c.id_paciente == paciente_id).values(id_paciente=data_update.id_paciente, nombre_paciente=data_update.nombre_paciente, apellidos_paciente=data_update.apellidos_paciente, numero_historial_clinico=data_update.numero_historial_clinico, domicilio=data_update.domicilio, poblacion_provincia=data_update.poblacion_provincia, telefono=data_update.telefono))
@@ -74,7 +74,7 @@ async def actualizar_paciente(data_update: PacienteSchema, paciente_id: int):
         return result
 
 #Borrar un paciente
-@user.delete("/pacientes/{paciente_id}", status_code=HTTP_204_NO_CONTENT, tags=["Pacientes"])
+@user.delete("/pacientes/{paciente_id}", status_code=HTTP_204_NO_CONTENT, tags=["Pacientes"], summary="Borra la información del paciente con id seleccionado")
 async def borrar_paciente(paciente_id: int):
     with engine.connect() as conn:
         conn.execute(paciente.delete().where(paciente.c.id_paciente == paciente_id))           #Borra el paciente en el que la columna (c) id paciente es la introducida
@@ -86,7 +86,7 @@ async def borrar_paciente(paciente_id: int):
 ####Tabla doctores
 
 #Buscar todos los doctores
-@user.get("/doctores", response_model=List[DoctorSchema], tags=["Doctores"])
+@user.get("/doctores", response_model=List[DoctorSchema], summary="Consulta todos los doctores", tags=["Doctores"])
 async def get_doctores():
  with engine.connect() as conn:
     result = conn.execute(doctor.select()).fetchall()
@@ -95,7 +95,7 @@ async def get_doctores():
     return result
 
 #Buscar doctores por id
-@user.get("/doctores/{doctor_id}", response_model=DoctorSchema, tags=["Doctores"])
+@user.get("/doctores/{doctor_id}", response_model=DoctorSchema, summary="Consulta el doctor con el id introducido", tags=["Doctores"])
 async def get_doctor(doctor_id: int):
     with engine.connect() as conn:
         result = conn.execute(doctor.select().where(doctor.c.id_doctor == doctor_id)).first()
@@ -107,7 +107,7 @@ async def get_doctor(doctor_id: int):
         return result    
 
 #Añadir nuevos doctores a la base de datos
-@user.post("/doctores", status_code=HTTP_201_CREATED, tags=["Doctores"])
+@user.post("/doctores", status_code=HTTP_201_CREATED, tags=["Doctores"], summary="Añade un nuevo paciente")
 async def crear_doctor(data_doctor: DoctorSchema):
     with engine.connect() as conn:
         nuevo_doctor = data_doctor.dict()
@@ -117,7 +117,7 @@ async def crear_doctor(data_doctor: DoctorSchema):
         return Response(status_code=HTTP_201_CREATED)
 
 #Actualizar información de un doctor ya existente
-@user.put("/doctores/{doctor_id}", status_code=HTTP_202_ACCEPTED, tags=["Doctores"])
+@user.put("/doctores/{doctor_id}", status_code=HTTP_202_ACCEPTED, tags=["Doctores"], summary="Actualiza la información de un doctor ya existente")
 async def actualizar_doctor(data_update: DoctorSchema, doctor_id: int):
     with engine.connect() as conn:
         result = conn.execute(doctor.update().where(doctor.c.id_doctor == doctor_id).values(id_doctor=data_update.id_doctor, nombre=data_update.nombre, apellidos=data_update.apellidos, sexo=data_update.sexo, fecha_nacimiento=data_update.fecha_nacimiento, especialidad=data_update.especialidad, numero_colegiado=data_update.numero_colegiado, cargo=data_update.cargo))
@@ -128,7 +128,7 @@ async def actualizar_doctor(data_update: DoctorSchema, doctor_id: int):
         return result
 
 #Borrar un doctor
-@user.delete("/doctores/{doctor_id}", status_code=HTTP_204_NO_CONTENT, tags=["Doctores"])
+@user.delete("/doctores/{doctor_id}", status_code=HTTP_204_NO_CONTENT, tags=["Doctores"], summary="Borra la información del doctor con id seleccionado")
 async def borrar_doctor(doctor_id: int):
     with engine.connect() as conn:
         conn.execute(doctor.delete().where(doctor.c.id_doctor == doctor_id))
@@ -140,7 +140,7 @@ async def borrar_doctor(doctor_id: int):
 #####Tabla citas
 
 #Buscar todas las citas
-@user.get("/codigo_cita", response_model=List[CodigocitaSchema], tags=["Citas"])
+@user.get("/codigo_cita", response_model=List[CodigocitaSchema], summary="Consulta todos las citas", tags=["Citas"])
 async def get_codigo_citas():
  with engine.connect() as conn:
     result = conn.execute(codigo_cita.select()).fetchall()
@@ -149,7 +149,7 @@ async def get_codigo_citas():
     return result
 
 #Buscar citas por fecha
-@user.get("/codigo_cita/{paciente_id}", response_model=CodigocitaSchema, tags=["Citas"])
+@user.get("/codigo_cita/{paciente_id}", response_model=CodigocitaSchema, summary="Consulta lacita del paciente con el id introducido", tags=["Citas"])
 async def get_codigo_cita( paciente_id: int):
     with engine.connect() as conn:
         result = conn.execute(codigo_cita.select().where(codigo_cita.c.id_paciente == paciente_id)).first()
@@ -161,7 +161,7 @@ async def get_codigo_cita( paciente_id: int):
         return result
 
 #Añadir nuevas citas a la base de datos
-@user.post("/codigo_cita", status_code=HTTP_201_CREATED, tags=["Citas"])
+@user.post("/codigo_cita", status_code=HTTP_201_CREATED, tags=["Citas"], summary="Añade una nueva cita")
 async def crear_codigo_cita(data_cita: CodigocitaSchema):
     with engine.connect() as conn:
         nueva_cita = data_cita.dict()
@@ -171,7 +171,7 @@ async def crear_codigo_cita(data_cita: CodigocitaSchema):
         return Response(status_code=HTTP_201_CREATED)
 
 #Actualizar información de una cita ya existente
-@user.put("/codigo_cita/{paciente_id}", status_code=HTTP_202_ACCEPTED, tags=["Citas"])
+@user.put("/codigo_cita/{paciente_id}", status_code=HTTP_202_ACCEPTED, tags=["Citas"], summary="Actualiza la información de una cita ya existente")
 async def actualizar_codigo_cita(data_update: CodigocitaSchema, paciente_id: int):
     with engine.connect() as conn:
         result = conn.execute(codigo_cita.update().where(codigo_cita.c.id_paciente == paciente_id).values(id_paciente=data_update.id_paciente, id_doctor=data_update.id_doctor, fecha=data_update.fecha, direccion=data_update.direccion))
@@ -182,7 +182,7 @@ async def actualizar_codigo_cita(data_update: CodigocitaSchema, paciente_id: int
         return result
 
 #Borrar una cita
-@user.delete("/codigo_cita/{paciente_id}", status_code=HTTP_204_NO_CONTENT, tags=["Citas"])
+@user.delete("/codigo_cita/{paciente_id}", status_code=HTTP_204_NO_CONTENT, tags=["Citas"], summary="Borra la información de la cita con id de paciente seleccionado")
 async def borrar_codigo_cita(paciente_id: int):
     with engine.connect() as conn:
         conn.execute(codigo_cita.delete().where(codigo_cita.c.id_paciente == paciente_id))
